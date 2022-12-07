071a8565af4281fee299e605ea8cf5719f34d98a
Hareesh Kumaran Navaneethakrishnan <hareeshkumaran.navaneethakrishnan@cesltd.com>
2022-12-07 23:43:11 +0530

    Solved with Single console.log statement

--- starPattern.js
+++ starPattern.js
@@ -1,15 +1,21 @@
+
+
 function starPattern(rowSize)
 {
+    let result="";
     for(let i=rowSize;i>0;i--)
     {
-        let str=""
-        for(let j=i;j>0;j--)
+        let str="";
+
+        for(let j=0;j<i;j++)
         {
           str=str+"*"
         }
-        console.log(str);
-
+         
+        result+=str+"\n"
+      
     }
+    console.log(result);
+   
 }
-
-starPattern(5)
+starPattern(5);
