4f3abfad81c3081f3b194ac9d787ad61d886c858
Hareesh Kumaran Navaneethakrishnan <hareeshkumaran.navaneethakrishnan@cesltd.com>
2022-12-08 21:55:35 +0530

    using bubble sort  logic

--- find_Kth_smallest_and_largest.js
+++ find_Kth_smallest_and_largest.js
@@ -9,8 +9,24 @@
 
 function findKthSmallestAndLargest(arr, k) {
   let result = {};
-  arr.sort();
+//   arr.sort();
 
+
+    //Custom sorting 
+
+    // using bubble sort
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+          if (arr[i] > arr[j]) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+          }
+        }
+      }
+
+    //End of custom sorting
+console.log(arr);
   let smallindex = largestindex = 0;
 
   smallindex = k - 1;
