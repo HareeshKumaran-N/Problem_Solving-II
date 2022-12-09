9dd54ebefbc432ed6e31eb6958477d4658eea5a2
Hareesh Kumaran Navaneethakrishnan <hareeshkumaran.navaneethakrishnan@cesltd.com>
2022-12-09 21:20:12 +0530

    Algorithm: using bubble sort

---
+++ ArraySort.js
@@ -0,0 +1,21 @@
+/*
+Sort an array of 0s, 1s and 2s
+
+input [1,2,1,1,1,0,0,0,1,2]
+output [0,0,0,1,1,1,1,2,1,2]
+
+input [1,0,1]
+output [0,1,1]
+
+input [2,0,1,2]
+output [0,1,2,2]
+*/
+
+/*
+Algorithm
+
+>>One approach is using sort method.(which I am gonna try).
+
+>>By using bubble sort 
+
+*/
